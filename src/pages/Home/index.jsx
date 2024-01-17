7b632207965bd4410f76341c121f703c8fd5c126
stephane_pachot <stef.pachot@gmail.com>
2024-01-17 09:10:07 +0100

    Initialisation du repository Git -Utilisation de create-react-app -Utilisation de react-router-dom -Utilisation de prop-types -Utilisation de SCSS -Utilisation de Prettier

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,25 @@
+import logo from '../../assets/logo.svg'
+import '../../css/Home/index.css'
+
+function Home() {
+	return (
+		<div className="Home">
+			<header className="Home-header">
+				<img src={logo} className="Home-logo" alt="logo" />
+				<p>
+					Edit <code>src/pages/Home/</code> and save to reload.
+				</p>
+				<a
+					className="Home-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	)
+}
+
+export default Home
